704f67b931dd633961cd75b67058f53bd4c763d0
Andrew Whiteside <andrew.whiteside@biqmind.com>
2021-01-05 12:36:57 +0800

    completed section 7

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,12 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useContext } from "react";
 import classes from "./Cockpit.css";
+import AuthContext from "../../context/auth-context";
 
 const cockpit = (props) => {
+  const toggleBtnRef = useRef(null);
+  const authContext = useContext(AuthContext);
+
+  console.log(authContext.authenticated);
+
   useEffect(() => {
     console.log("[Cockpit.js] useEffect");
-    setTimeout(() => {
-      alert("Saved data to cloud!");
-    }, 1000);
+    // setTimeout(() => {
+    //   alert("Saved data to cloud!");
+    // }, 1000);
+    toggleBtnRef.current.click();
     return () => {
       console.log("[Cockpit.js] cleanup work in useEffect");
     };
@@ -24,21 +31,22 @@ const cockpit = (props) => {
   if (props.showPersons) {
     btnClass = classes.Red;
   }
-  if (props.persons.length <= 2) {
+  if (props.personsLength <= 2) {
     assignedClasses.push(classes.red); // assignedClasses = ['red']
   }
-  if (props.persons.length <= 1) {
+  if (props.personsLength <= 1) {
     assignedClasses.push(classes.bold); // assignedClasses = ['red', 'bold']
   }
   return (
     <div className={classes.Cockpit}>
       <h1>{props.title}</h1>
       <p className={assignedClasses.join(" ")}>This is really working!</p>
-      <button className={btnClass} onClick={props.clicked}>
+      <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
         Toggle Persons
       </button>
+      <button onClick={authContext.login}>Log in</button>
     </div>
   );
 };
 
-export default cockpit;
+export default React.memo(cockpit);
